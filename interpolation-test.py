7f75d70ba211cf0217153d57fa5ecd5f8addc2f9
JC <node.zerocool@gmail.com>
2020-07-27 13:40:51 -0500

    full int

--- interpolation-test.py
+++ interpolation-test.py
@@ -1,10 +1,32 @@
+#class Person:
+#  def __init__(self, name, age):
+#    self.name = name
+#    self.age = age
+
+#  def myFunc(self):
+#    print("My age is {}".format(self.age))
+
+#p1 = Person("Jorge", 34)
+#p1.myFunc()	
+
 class Person:
   def __init__(self, name, age):
     self.name = name
     self.age = age
 
-  def myFunc(self):
-    print("My age is {}".format(self.age))
+  def myfunc(self):
+    print("Hello my name is " + self.name)
 
-p1 = Person("Jorge", 34)
-p1.myFunc()	
+p1 = Person("John", 36)
+p1.myfunc()
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("and my age is {}".format(self.age))
+
+p1 = Person("John", 36)
+p1.myfunc()
