b191762b74681424ef4db4b662dcdacc244d2eec
JC <node.zerocool@gmail.com>
2020-07-29 19:04:09 -0500

    code

---
+++ Patterns_Python/builder.py
@@ -0,0 +1,13 @@
+class Director():
+    """Director"""
+    def __init_(self, builder):
+        self.builder = builder
+
+    def construct_car(self):
+        self._builder.create_new_car()
+        self._builder.add_model()
+        self._builder.add_tires()
+        self._builder.add_engine()
+
+    def get_car(self):
+        return self._builder.car
