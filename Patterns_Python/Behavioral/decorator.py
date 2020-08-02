aac0e3a8ca57257e8aab679a03c3f4811ce1550e
JC <node.zerocool@gmail.com>
2020-08-02 01:18:41 -0500

    blink

--- Patterns_Python/Behavioral/decorator.py
+++ Patterns_Python/Behavioral/decorator.py
@@ -4,3 +4,14 @@ def make_blink(function):
     """Defines the decorator"""
 
     #This makes the decorator transparent in terms of its name and docstring
+    @wraps(function)
+
+    #Define the inner function
+    def decorator():
+        #Grab the return value of the function being decorated
+        ret = function()
+
+        #Add new functionality to the function being decorate
+        return "<blink>" + ret + "</blink>"
+
+    return decorator
