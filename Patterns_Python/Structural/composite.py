939e9150062cb75783b1d0a363b3565c2ed90e74
JC <node.zerocool@gmail.com>
2020-08-06 22:04:22 -0500

    comp

--- Patterns_Python/Structural/composite.py
+++ Patterns_Python/Structural/composite.py
@@ -3,3 +3,22 @@ class Component(object):
 
     def __init(self, *args, **kwargs):
         pass
+
+    def component_function(self):
+        pass
+
+class Child(Component):
+    """Concrete class"""
+
+    def __init__(self, *args, *kwargs):
+        Component.__init__(self, *args, **kwargs)
+
+        #This is where we store the name of your child item!
+        self.name = args[0]
+
+    def component_function(self):
+        #Print the name of your child item here!
+        print("{}".format(self.name))
+
+class Composite(Component): #Inherits from the abstract class, Component
+    """Concrete class and maintains the tree resursive structure"""
