a26f2c4b702f34b1f82cb87cbd0a3f7f3839deaa
JC <node.zerocool@gmail.com>
2020-08-08 13:52:30 -0500

    bridge

--- Patterns_Python/Structural/bridge.py
+++ Patterns_Python/Structural/bridge.py
@@ -1,1 +1,5 @@
 class DrawingAPIone(object):
+    """Implementation-specific abstraction: concrete class one"""
+    def draw_circle(self, x, y, radius):
+        print("API 1 drawinga  circle at ({}, {} with radius {}!)".format(x, y, radius))
+        
