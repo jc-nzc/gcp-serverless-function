b89851e28eb6b246d866785bde8c192aaf1e0234
JC <node.zerocool@gmail.com>
2020-08-10 18:07:49 -0500

    proxy fin

--- Patterns_Python/Structural/proxy.py
+++ Patterns_Python/Structural/proxy.py
@@ -13,4 +13,32 @@ class Proxy:
     def __init__(self):
         self.occupied = 'No'
         self.producer = None
-    
+
+    def produce(self):
+        """Check if Producer is available"""
+        print("Artist checking if Producer is available ...")
+
+        if self.occupied == 'No':
+            #If the producer is available, create a producer object!
+            self.producer = Producer()
+            time.sleep(2)
+
+            #Make the producer meet the guest!
+            self.producer.meet()
+
+        else:
+            #otherwise, don't instantiate a producer
+            time.sleep(2)
+            print("Producer is busy!")
+
+#Instantiate a Proxy
+p = Proxy()
+
+#Make the proxy: Artist produce until Producer is available
+p.produce()
+
+#Change the state to 'occupied'
+p.occupied = 'Yes'
+
+#Make the Producer produce
+p.produce()
