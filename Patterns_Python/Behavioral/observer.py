97b2651ef85ef1e99e5e7996b1c06e8e72574bda
JC <node.zerocool@gmail.com>
2020-08-11 18:23:09 -0500

    obs

--- Patterns_Python/Behavioral/observer.py
+++ Patterns_Python/Behavioral/observer.py
@@ -7,3 +7,35 @@ class Subject(object): #Represents what is being 'observed'
     def attach(self, observer):
         if observer not in self._observers:
             self._observers.append(observer)
+
+    def detach(self, observer):
+        try:
+            self._observers.remove(observer)
+        except ValueError:
+            pass
+
+    def notify(self, modifier=None):
+        for observer in self._observers:
+            if modifier != observer:
+                observer.update(self)
+
+class Core(Subject):
+
+    def __init__(self, name-""):
+        Subject._name = name
+        self._name = name
+        self._temp = 0
+
+    @property
+    def temp(self):
+        return self._temp
+
+    @temp.setter
+    def temp(self, temp):
+        self._temp = temp
+        self.notify()
+
+class TempViewer:
+
+    def update(self, subject):
+        print("Temperature Viewer: {} has Temperature {}".format(subject._name, subject._temp))
