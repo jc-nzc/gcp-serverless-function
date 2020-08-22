59bbd0a6883cc4b9e865155bfb4e2a61c3a79d09
JC <node.zerocool@gmail.com>
2020-08-21 21:43:55 -0500

    visitor appended

--- Patterns_Python/Behavioral/visitor.py
+++ Patterns_Python/Behavioral/visitor.py
@@ -3,4 +3,25 @@ class House(object):
         """Interface to accept a visitor"""
         visitor.visit(self)
 
-    
+    def work_on_hvac(self, hvac_specialist):
+        print(self, "worked on by", hvac_specialist)
+
+    def work_on_electricity(self, electrician):
+        print(self, "worked on by", electrician)
+
+    def __str__(self):
+        """Simply return the class name when the House object is printed"""
+        return self.__class__.__name__
+
+
+class Visitor(object):
+    """Abstract visitor"""
+    def __str__(self):
+        """Simply return the class name when the Visitor object is printed"""
+        return self.__class__.__name__
+
+
+class HvacSpecialist(Visitor):
+    """Concrete visitor: HVAC specialist"""
+    def visit(self, house):
+        house.work_on_hvac(self)
