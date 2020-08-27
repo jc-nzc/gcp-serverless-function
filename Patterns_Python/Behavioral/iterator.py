a8a4af170fb75a423aeec7f735c1287ca93de703
JC <node.zerocool@gmail.com>
2020-08-26 22:17:30 -0500

    iterator file

---
+++ Patterns_Python/Behavioral/iterator.py
@@ -0,0 +1,24 @@
+def count_to(count):
+    """Our iterator implementation"""
+
+
+    numbers_in_german = ["eins", "zwei", "drei", "vier", "funf"]
+
+
+
+    iterator = zip(range(count), numbers_in_german)
+
+
+
+
+    for position, number in iterator:
+
+
+        yield number
+
+
+for num in count_to(3):
+    print("{}".format(num))
+
+for num in count_to(4):
+    print("{}".format(num))
