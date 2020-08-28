5bbbf99190cba148be8be8cb1e422d4ddc37ecdc
JC <node.zerocool@gmail.com>
2020-08-28 17:44:14 -0500

    file with error

--- Patterns_Python/Behavioral/chain.py
+++ Patterns_Python/Behavioral/chain.py
@@ -12,3 +12,37 @@ class Handler:
 
     def _handle(self, request):
         raise NotImplementedError('Must provide implementation in sublass!')
+
+class ConcreteHandler1(Handler):
+    """Concrete handler 1"""
+    def _handle(self, request):
+        if 0 < request <= 10:
+            print("Request {} handled in handler 1".format(request))
+            return True
+
+class DefaultHandler(Handler):
+    """Default handler"""
+
+    def _handler(self, request):
+        """If there is no handler available"""
+
+        print("End of chain, no handler for {}".format(request))
+        return True
+
+class Client:
+    def __init__(self):
+        self.handler = ConcreteHandler1(DefaultHandler(None))
+
+
+    def delegate(self, requests):
+        for request in requests:
+            self.handler.handle(request)
+
+
+c = Client()
+
+
+requests = [2, 5, 30]
+
+
+c.delegate(requests)
