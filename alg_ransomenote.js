9058a5917f0dbfa0490a1721345578070cf5daf9
JC <node.zerocool@gmail.com>
2020-09-02 23:59:10 -0500

    Big O

---
+++ alg_ransomenote.js
@@ -0,0 +1,18 @@
+// Time Complexity and Big O Notation
+
+// Constant RunTime O(1) will only execute the same number of operations regardless of inputs
+
+// Linear RunTime O(n) operations increase proportionally to the input ; o for function, n for size of the input
+
+// Exponential RunTime O(n^2) evaluate all possibilities; for every added input, it takes an exponential jump; inefficient, slow, avoid
+
+// Logrithmic RunTime O(n²) Ex. Binary Search, Sorted, 2 inputs, reduces throughout the search by cutting the input in half
+
+
+
+
+function harmlessRandsomNote(noteText, magazineText){
+  var noteArr = noteText.split('');
+  var magazineArr = magazineText.split('');
+  
+}
