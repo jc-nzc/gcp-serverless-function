1238cca2f7247d88ed9d61e0641ebce48c359899
JC <node.zerocool@gmail.com>
2020-09-03 00:09:55 -0500

    harmless

--- alg_ransomenote.js
+++ alg_ransomenote.js
@@ -12,7 +12,17 @@
 
 
 function harmlessRandsomNote(noteText, magazineText){
-  var noteArr = noteText.split('');
-  var magazineArr = magazineText.split('');
-  
+  var noteArr = noteText.split(' ');
+  var magazineArr = magazineText.split(' ');
+  var magazineObj = {};
+
+  magazineArr.forEach(word => {
+    if(!magazineObj[word]) magazineObj[word] = 0;
+    magazineObj[word]++;
+  });
+
+  console.log(magazineObj);
+
 }
+
+harmlessRandsomNote('', 'this is all the magazine text in the magazine');
