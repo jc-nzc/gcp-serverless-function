cb7a88dd4ae7c3b33dc425fcb82d646aca683f7a
JC <node.zerocool@gmail.com>
2020-09-20 23:13:36 -0500

    output

--- alg_two_sum.js
+++ alg_two_sum.js
@@ -1,7 +1,7 @@
 function twoSum(numArray, sum) {
   var pairs = [];
   var hashTable = [];
- 
+
   for (var i = 0; i < numArray.length; i++) {
     var currNum = numArray[i];
     var counterpart = sum - currNum;
@@ -10,8 +10,8 @@ function twoSum(numArray, sum) {
     }
     hashTable.push(currNum);
   }
-  
+
   return pairs;
 }
- 
-twoSum([1, 6, 4, 5, 3, 3], 7);
+
+console.log(twoSum([1, 6, 4, 5, 3, 3], 7));
