bf8d35168d20331414d271b95dfed0acd4de3245
JC <node.zerocool@gmail.com>
2020-09-21 20:46:31 -0500

    log

--- alg_binary_search.js
+++ alg_binary_search.js
@@ -1,7 +1,7 @@
 function binarySearch(numArray, key) {
     var middleIdx = Math.floor(numArray.length / 2);
     var middleElem = numArray[middleIdx];
-    
+
     if (middleElem === key) return true;
     else if (middleElem < key && numArray.length > 1) {
         return binarySearch(numArray.splice(middleIdx, numArray.length), key);
@@ -11,5 +11,5 @@ function binarySearch(numArray, key) {
     }
     else return false;
 }
- 
-binarySearch([5, 7, 12, 16, 36, 39, 42, 56, 71], 56);
+
+console.log(binarySearch([5, 7, 12, 16, 36, 39, 42, 56, 71], 56));
