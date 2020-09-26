0ba36b8a2a290f78945b4b8f7bc486828f1c99e1
JC <node.zerocool@gmail.com>
2020-09-25 19:08:22 -0500

    func and for loops

---
+++ alg_sieve_of_aratosthenes.js
@@ -0,0 +1,16 @@
+function sieveOfEratosthenes(n) {
+  var primes = [];
+  for (var i = 0; i <= n; i++) {
+  primes[i] = true;
+  }
+  
+  primes[0] = false;
+  primes[1] = false;
+
+  for (var i = 2; i <= Math.sqrt(n); i++{
+    for (j = 2; i * j <= n; j++{
+    primes[i * j] = false;
+    }
+  }
+
+}
