9c889080b2948d6a156a8aa99839b6249739949d
JC <node.zerocool@gmail.com>
2020-09-29 18:33:02 -0500

    func complete

--- alg_bubblesort.js
+++ alg_bubblesort.js
@@ -1,3 +1,14 @@
 function bubbleSort(array){
-  
+    for (var i = array.length; i > 0; i--) {
+      for (var j = 0; j < i; j++) {
+        if(array[j] > array[j + 1]){
+          var temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+        }
+      }
+    }
+    return array;
 }
+
+bubbleSort([6000, 34, 203, 3, 746, 200, 984, 100, 764, 16, 34, 1]);
