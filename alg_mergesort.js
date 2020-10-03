d8a7b743b58447bee23cc2e93366d8431bfb4449
JC <node.zerocool@gmail.com>
2020-10-02 23:06:03 -0500

    fixed error )

--- alg_mergesort.js
+++ alg_mergesort.js
@@ -10,15 +10,15 @@ function mergeSort (arr) {
 function merge (array1, array2) {
     var result = [];
     while (array1.length && array2.length) {
-    var minElem:
-    if (array1[0] < array2[0] minElem = array1.shift();
-    else minElem = array2.shift();
-    return.push(minElem);
-  }
-  
-  if(array1.length) result = result.concat(array1);
-  else result = result.concat(array2);  
-  return result;
+      var minElem;
+      if (array1[0] < array2[0]) minElem = array1.shift();
+      else minElem = array2.shift();
+      result.push(minElem);
+    }
+
+    if (array1.length) result = result.concat(array1);
+    else result =result.concat(array2);
+    return result;
 }
 
-mergeSort([6000, 34, 203, 3, 746, 200, 984, 198, 764, 1, 9, 1]);
+mergeSort([6000, 34, 203, 3, 746, 200, 984, 198, 764, 1, 9, 1])
