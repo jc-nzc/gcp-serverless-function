826513f617b9aabb00ed228e921d0011376fa7d3
JC <node.zerocool@gmail.com>
2020-10-07 21:11:11 -0500

    update to python3 ()

--- linked_list.py
+++
@@ -1,6 +0,0 @@
-# Declare a dictionary 
-dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
-
-# Accessing the dictionary with its key
-print "dict['Name']: ", dict['Name']
-print "dict['Age']: ", dict['Age']
