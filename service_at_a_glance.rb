71f5d299fc730fda2fbf452beb94579debaa9b3d
Zero <node.zerocool@gmail.com>
2021-02-14 22:09:20 -0600

    add

--- service_at_a_glance.rb
+++ service_at_a_glance.rb
@@ -13,3 +13,25 @@ class Handler
     Clock::UTC.configure(self)
     Store.configure(self)
   end
+
+  category :account
+
+  handle Withdraw do |withdraw|
+    account_id = withdraw.account_id
+
+    account = store.fetch(account_id)
+
+    time = clock.iso8601
+
+    stream_name = stream_name(account_id)
+
+    unless account.sufficient_funds?(withdraw.amount)
+      withdrawal_rejected = WithdrawalRejected.follow(withdraw)
+      withdrawal_rejected.time = time
+
+      write.(withdrawal_rejected, stream_name)
+
+      return
+    end
+
+    
