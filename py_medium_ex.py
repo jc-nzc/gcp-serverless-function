a26e08ab221e07b10b4b37e50b3305dc5b67dd72
Zero <node.zerocool@gmail.com>
2021-02-16 23:16:57 -0600

    ex

---
+++ py_medium_ex.py
@@ -0,0 +1,34 @@
+{
+  "avatar": "1*Y7zH0UM975YmchIO86uIGA.jpeg",
+  "bio": "Mixtape of developer, designer and startup. Cofounder and developer of DualCores Studio. Follow my technical blog: http://enginebai.logdown.com/",
+  "display_name": "Engine Bai",
+  "facebook": "789985027713671",
+  "followedby_count": 445,
+  "following_count": 238,
+  "publications": [
+    {
+      "creator_user_id": "3301d32a6bba",
+      "description": "Stories from the mix of designer and developer. 設計與工程的交織，混搭激盪出不同的想像。",
+      "display_name": "DualCores Studio",
+      "follower_count": 302,
+      "image": {
+        "image_id": "1*DLixNgsMpK5B74na3EDucQ.png",
+        "original_height": 591,
+        "original_width": 591
+      },
+      "logo": {
+        "image_id": "1*DLixNgsMpK5B74na3EDucQ.png",
+        "original_height": 591,
+        "original_width": 591
+      },
+      "name": "dualcores-studio",
+      "post_count": 0,
+      "publication_id": "275e26e7c1b2",
+      "url": "https://medium.com/dualcores-studio"
+    },
+    ...more
+  ],
+  "twitter": "enginebai",
+  "user_id": "3301d32a6bba",
+  "username": "enginebai"
+}
